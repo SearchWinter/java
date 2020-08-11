c04a8d190f5b68da096d6751e47cf9a64c942049
SearchWinter <QWER123qwer938$+++>
2020-08-11 16:48:57 +0800

    线程安全和线程不安全比较

--- src/main/java/thread/block/StorageBlock2.java
+++ src/main/java/thread/block/StorageBlock2.java
@@ -1,5 +1,6 @@
 package thread.block;
 
+
 import java.util.*;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -8,26 +9,27 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @Description 数据存储载体 生产和消费方法
  **/
 public class StorageBlock2 {
-    // 仓库存储的载体
-    private Queue<Integer> queue = new ArrayDeque<>(10);
-    //整形原子类，多个线程操作同一数据保持顺序；前一线程还未结束，后一线程看到的数据可能是还未更改的
-    //https://www.cnblogs.com/aimei/p/12200367.html
-    private AtomicInteger start =new AtomicInteger(0);
+    // 仓库存储的载体  线程安全
+    public static LinkedBlockingQueue<Integer> lbqueue = new LinkedBlockingQueue<>();
+    //非线程安全
+    public static Queue<Integer> queue = new ArrayDeque<>(10);
 
-    public void produce() {
-        //自增
+    //整形原子类，多个线程操作同一数据保持顺序；前一线程还未结束，后一线程看到的数据可能是还未更改的
+    private AtomicInteger start = new AtomicInteger(0);
+
+    public void produce() throws InterruptedException {
+        //获取当前值，并自增
         int i = start.getAndIncrement();
-        queue.add(i);
-        System.out.println("【生产" + Thread.currentThread().getName() + "】生产一个产品，现库存" + queue.size());
+//        queue.add(i);
+        lbqueue.put(i);
     }
 
-    public void consume() {
+    public void consume() throws InterruptedException {
         //返回并移除队列的第一个元素
-        if(!queue.isEmpty()) {
-            queue.poll();
-            System.out.println("【消费者" + Thread.currentThread().getName() + "】消费了一个产品,现库存" + queue.size());
-        }else {
-            System.out.println(Thread.currentThread().getName()+"库存为0，请等待。");
+        if (!queue.isEmpty()) {
+//            Integer poll = queue.poll();
+//            System.out.println(poll);
+            lbqueue.take();
         }
     }
 }
