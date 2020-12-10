4cb2d86874d97c79a1c6062536f07f143ed6272a
anjunli <anjunli@upchina.com>
2020-12-10 11:37:47 +0800

    测试案例

--- src/main/java/TestDemo.java
+++ src/main/java/TestDemo.java
@@ -1,39 +1,65 @@
 import org.junit.Test;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 /**
  * @Description if后面可以不接else,只有当if(true)的时候才会执行
- *              if()中是一个方法时，哪怕结果为false,（）中的方法还是会执行;list作为参数，值已经改变
- * @Date 2020/8/25  14:17
- **/
+ * if()中是一个方法时，哪怕结果为false,（）中的方法还是会执行;list作为参数，值已经改变 @Date 2020/8/25 14:17
+ */
 public class TestDemo {
-    public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
+  public static void main(String[] args) {
+    List<Integer> list = new ArrayList<>();
 
-        if(!putDemo(list)){
-            System.out.println("if条件为false,作为参数的list的值，会改变");
+    if (!putDemo(list)) {
+      System.out.println("if条件为false,作为参数的list的值，会改变");
+    }
+    System.out.println(list);
+  }
+
+  public static boolean putDemo(List<Integer> list) {
+    list.add(1);
+    list.add(2);
+    list.add(2);
+    list.add(2);
+    return true;
+  }
+
+  @Test
+  public void randomTest() {
+    Random random = new Random();
+    for (int j = 0; j < 10; j++) {
+      int i = random.nextInt(20);
+      System.out.println(i);
+    }
+  }
+
+  @Test
+  public void UTest1() throws ParseException {
+    String beforeNDay = Utils.getBeforeNDay("20201104", 2);
+    System.out.println(beforeNDay);
+  }
+
+  @Test
+    public void IFContinue(){
+    for (int i = 0; i < 10 ;i++) {
+      //
+        if (i==5){
+            continue;
         }
-        System.out.println(list);
+      System.out.println(i);
     }
+  }
 
-    public static boolean putDemo(List<Integer> list){
-        list.add(1);
-        list.add(2);
-        list.add(2);
-        list.add(2);
-        return true;
-    }
+  @Test
+  public void testNull(){
+    System.out.println("taf"+null);
+  }
 
-    @Test
-    public void randomTest(){
-        Random random = new Random();
-        for (int j = 0; j <10 ; j++) {
-            int i = random.nextInt(20);
-            System.out.println(i);
-        }
-    }
+  @Test
+  public void math(){
+    System.out.println(Math.round(Math.random()*1000000));
+  }
 }
-
